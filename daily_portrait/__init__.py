57c7c1de811f395b8074c69ed41ad6f04f2da587
vicalloy <zbirder@gmail.com>
2023-02-02 14:04:09 +0800

    add images_to_gif

--- daily_portrait/__init__.py
+++ daily_portrait/__init__.py
@@ -1,2 +1,2 @@
 # flake8: noqa
-from alignment import align_faces
+from .alignment import align_faces
--- daily_portrait/utils.py
+++ daily_portrait/utils.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 import cv2
+from PIL import Image
 
 
 def images_to_video(input_dir: Path, output: Path, frame_size: tuple[int, int]):
@@ -10,3 +11,19 @@ def images_to_video(input_dir: Path, output: Path, frame_size: tuple[int, int]):
         img = cv2.resize(img, frame_size)
         out.write(img)
     out.release()
+
+
+def images_to_gif(input_dir: Path, output: Path, frame_size: tuple[int, int]):
+    images = []
+    for img_filename in input_dir.glob("*.jpg"):
+        with Image.open(img_filename) as img:
+            img = img.resize(frame_size)
+            images.append(img)
+    images[0].save(
+        str(output),
+        save_all=True,
+        append_images=images[1:],
+        optimize=True,
+        duration=200,
+        loop=0,
+    )
