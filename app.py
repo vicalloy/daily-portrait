e8846e8e592805fe107d1de447aa59396e29c104
vicalloy <zbirder@gmail.com>
2023-03-31 15:32:59 +0800

    add config_logging

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import logging
 from pathlib import Path
 
 from daily_portrait import process_images
 from daily_portrait.utils import images_to_gif, rename_photos
 
+
+def config_logging():
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d %(message)s",
+        handlers=[logging.StreamHandler()],
+    )
+
+
 if __name__ == "__main__":
+    config_logging()
     BASE_DIR = Path(__file__).resolve().parent
     rename_photos(BASE_DIR / "input")
     process_images(BASE_DIR / "input", BASE_DIR / "output")
--- daily_portrait/alignment.py
+++ daily_portrait/alignment.py
@@ -1,3 +1,4 @@
+import logging
 import math
 from typing import Iterable, TypeAlias
 
@@ -8,6 +9,7 @@ import numpy as np
 Point: TypeAlias = tuple[int, int]
 standard_center_point: Point | None = None
 standard_eyes_dist: float | None = None
+logger = logging.getLogger(__name__)
 
 
 def points_center(points: Iterable[Point]) -> Point:
@@ -27,27 +29,27 @@ def get_rotation_matrix(p1: Point, p2: Point, scale):
     angle = angle_between_2_points(p1, p2)
     center_point = points_center([p1, p2])
     xc, yc = center_point
-    print("center:", center_point)
+    logger.debug("center:", center_point)
     # TODO calculate scale
     return cv2.getRotationMatrix2D((xc, yc), angle, scale)
 
 
 def get_eyes_points(image: np.ndarray):
     face_landmarks: np.ndarray = face_recognition.face_landmarks(image)
-    print("find face count:", len(face_landmarks))
+    logger.debug("find face count:", len(face_landmarks))
     assert len(face_landmarks) == 1
     face_landmarks = face_landmarks[0]
 
     # print(face_landmarks)
     left_eye = points_center(face_landmarks["left_eye"])
     right_eye = points_center(face_landmarks["right_eye"])
-    print("eyes:", left_eye, right_eye)
+    logger.debug("eyes:", left_eye, right_eye)
     return left_eye, right_eye
 
 
 def get_scale(left_eye, right_eye):
     dist = math.dist(left_eye, right_eye)
-    print("dist:", dist)
+    logger.debug("dist:", dist)
     scale = 1
     global standard_eyes_dist
     if standard_eyes_dist:
@@ -62,14 +64,14 @@ def rotation_face(
 ):
     scale = get_scale(left_eye, right_eye)
     matrix = get_rotation_matrix(left_eye, right_eye, scale)
-    print("matrix:", matrix)
+    logger.debug("matrix:", matrix)
     return cv2.warpAffine(image, matrix, (width, height), flags=cv2.INTER_CUBIC)
 
 
 def get_move_vector(left_eye: Point, right_eye: Point):
     global standard_center_point
     center_point = points_center([left_eye, right_eye])
-    print("center:", center_point)
+    logger.debug("center:", center_point)
     move_x, move_y = 0, 0
     if standard_center_point:
         move_x = standard_center_point[0] - center_point[0]
